4bb81498ad3e075073bb1082cb17a031d74a9869
electronicsworlds <77326473+electronicsworlds@users.noreply.github.com>
2021-01-12 16:11:54 +0530

    Add files via upload

---
+++ Sound_sensor.ino
@@ -0,0 +1,22 @@
+#include <LCD_I2C.h>
+LCD_I2C lcd(0x27);   // Default address of most PCF8574 modules, change according
+int AudioPin = A0;   // Connect Your Analog Sound Sensor Module To This Pin
+int value;       // Variable to store the sensor input value
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(); 
+  lcd.backlight();
+  pinMode(AudioPin,INPUT);  // Set the pin to read from sensor
+}
+
+void loop() {
+  value = analogRead (AudioPin);  // Reading the sensor data
+    if (value>50){
+    lcd.setCursor(5,0);
+    lcd.print("Keep");
+    lcd.setCursor(5,1);
+    lcd.print("Quite");
+    delay(1000);
+    lcd.clear();
+    }
+}
