e8c150ba9308efdf7cb7cd0c3d916e4775f07122
electronicsworlds <77326473+electronicsworlds@users.noreply.github.com>
2021-01-12 16:22:52 +0530

    Update Sound_sensor.ino
    This is the code for the analog sound sensor module. Just connect the module according to the program and upload to the arduino and get ready to detect the sound from the sensor. Feel free to change it according to your requirement.

--- Sound_sensor.ino
+++ Sound_sensor.ino
@@ -3,19 +3,19 @@ LCD_I2C lcd(0x27);   // Default address of most PCF8574 modules, change accordin
 int AudioPin = A0;   // Connect Your Analog Sound Sensor Module To This Pin
 int value;       // Variable to store the sensor input value
 void setup() {
-  lcd.begin(); 
-  lcd.backlight();
+  lcd.begin();   // Initializing LCD Module
+  lcd.backlight();  // Initializing LCD Screen Backlight
   pinMode(AudioPin,INPUT);  // Set the pin to read from sensor
 }
 
 void loop() {
   value = analogRead (AudioPin);  // Reading the sensor data
-    if (value>50){
-    lcd.setCursor(5,0);
-    lcd.print("Keep");
-    lcd.setCursor(5,1);
-    lcd.print("Quite");
-    delay(1000);
-    lcd.clear();
+    if (value>50){          // Initializing the minimum sensitivity value
+    lcd.setCursor(5,0);   // Set the screen print location
+    lcd.print("Keep");    // Printing the message
+    lcd.setCursor(5,1);   // Set the screen print location
+    lcd.print("Quite");  // Printing the message
+    delay(1000);   // Providing delay for the message
+    lcd.clear();  // Clear the lcd screen for next printing
     }
 }
